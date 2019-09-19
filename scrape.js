98f0392cada8d4e42493c222765427b8140c2199
maxsteele2314 <54860529+maxsteele2314@users.noreply.github.com>
2019-09-19 15:41:21 -0400

    Update scrape.js

--- scrape.js
+++ scrape.js
@@ -2,4 +2,16 @@ var scraper = require('./tools').scraper;
 //scraper = function(url,parseFcn,filename)
 //scrapes a URL, parses it with parseFcn, and archives the data in a JSON file called filename.
 
+scraper("https://www.marvel.com/comics/characters", avengers,"scrape-file")
+
+var avengers = function($,data)
+{
+    var parent = $("#comics-characterindex > section.module.moduColor_Light.modu_AZ > div:nth-child(4) > div");
+    var children = parent.find("li")
+    children.toArray()
+     .forEach(function(item,index)
+{
+     data.push($(item).text());   
+   });    
+ }
 
